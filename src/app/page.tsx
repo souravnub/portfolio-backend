be1f99792204d2ac086dc8cf9f90be878b2d7690
Sourav <s6hr6h4u3@gmail.com>
2024-07-09 16:04:00 -0600

    init: initialize shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
     return (
         <main>
             <h1>hello</h1>
+            <Button>Hello world</Button>
         </main>
     );
 }
