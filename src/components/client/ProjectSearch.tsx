5d6e0e829556e8f59d0b0313907a261e4a668ea5
Sourav <s6hr6h4u3@gmail.com>
2024-07-14 22:48:06 -0600

    feat: clear input feature on projects search input

--- src/components/client/ProjectSearch.tsx
+++ src/components/client/ProjectSearch.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-import { Search } from "lucide-react";
+import { Cross, Search } from "lucide-react";
 import { Input } from "../ui/input";
 import { useEffect, useState } from "react";
 import { useDebounce } from "@uidotdev/usehooks";
 import { useRouter } from "next/navigation";
+import { MdClose } from "react-icons/md";
+import { Button } from "../ui/button";
 
 export function ProjectSearch({
     initialSearchQuery,
@@ -30,6 +32,14 @@ export function ProjectSearch({
                 placeholder="Search Projects..."
                 className="w-full rounded-lg bg-background pl-8 py-5"
             />
+            {input.length > 0 && (
+                <Button
+                    variant={"ghost"}
+                    className="p-0 absolute right-0 top-1/2 -translate-y-1/2  h-full px-3 group hover:bg-transparent"
+                    onClick={() => setInput("")}>
+                    <MdClose className="group-hover:text-primary text-primary/40" />
+                </Button>
+            )}
         </div>
     );
 }
