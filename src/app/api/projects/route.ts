ea7a9b593d28d4079098f4d6ee678dd2ad4ce683
souravnub <s6hr6h4u3@gmail.com>
2024-08-15 17:31:32 -0600

    refactor (api): limit on projects

--- src/app/api/projects/route.ts
+++ src/app/api/projects/route.ts
@@ -1,12 +1,19 @@
 import prisma from "@/db";
+
 export async function GET(request: Request, res: Response) {
+    const { searchParams } = new URL(request.url);
+
     const projects = await prisma.project.findMany({
         where: { isPublished: true },
         orderBy: { createdAt: "desc" },
     });
 
+    const count = searchParams.get("count");
+
+    const finalProjects = count ? projects.slice(0, Number(count)) : projects;
+
     return Response.json(
-        { projects },
+        { projects: finalProjects },
         {
             headers: {
                 "Access-Control-Allow-Origin":
