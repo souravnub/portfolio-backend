feac0cfc58b4f0c259506b4ea9f31c8723102c05
souravnub <s6hr6h4u3@gmail.com>
2024-08-21 23:54:36 -0600

    fix(prod): fixed authentication error while logging in

--- next.config.mjs
+++ next.config.mjs
@@ -10,27 +10,6 @@ const nextConfig = {
             },
         ],
     },
-    async headers() {
-        return [
-            {
-                source: "/api/:path*",
-                headers: [
-                    {
-                        key: "Access-Control-Allow-Origin",
-                        value: process.env.NEXT_PUBLIC_FRONTEND_URL,
-                    },
-                    {
-                        key: "Access-Control-Allow-Methods",
-                        value: "GET, POST, PUT, DELETE, OPTIONS",
-                    },
-                    {
-                        key: "Content-type",
-                        value: "application/json",
-                    },
-                ],
-            },
-        ];
-    },
 };
 
 export default nextConfig;
--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -12,10 +12,19 @@ export async function POST(request: Request, res: Response) {
     const validationRes = contactSchema.safeParse(body);
 
     if (!validationRes.success) {
-        return Response.json({
-            success: false,
-            message: "Input validation failed",
-        });
+        return Response.json(
+            {
+                success: false,
+                message: "Input validation failed",
+            },
+            {
+                headers: {
+                    "Access-Control-Allow-Origin":
+                        process.env.NEXT_PUBLIC_FRONTEND_URL!,
+                    "Content-Type": "application/json",
+                },
+            }
+        );
     }
 
     try {
@@ -27,14 +36,32 @@ export async function POST(request: Request, res: Response) {
             },
         });
 
-        return Response.json({
-            success: true,
-            message: "Message received",
-        });
+        return Response.json(
+            {
+                success: true,
+                message: "Message received",
+            },
+            {
+                headers: {
+                    "Access-Control-Allow-Origin":
+                        process.env.NEXT_PUBLIC_FRONTEND_URL!,
+                    "Content-Type": "application/json",
+                },
+            }
+        );
     } catch (err) {
-        return Response.json({
-            success: false,
-            message: "Error while saving message! Please try again.",
-        });
+        return Response.json(
+            {
+                success: false,
+                message: "Error while saving message! Please try again.",
+            },
+            {
+                headers: {
+                    "Access-Control-Allow-Origin":
+                        process.env.NEXT_PUBLIC_FRONTEND_URL!,
+                    "Content-Type": "application/json",
+                },
+            }
+        );
     }
 }
